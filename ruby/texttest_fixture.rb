3b756c428daa026549d4803a81f06fe1101e737d
Masoudi Parrot <masoudi@localhost.localdomain>
2022-12-16 02:43:12 -0500

    Use require relative instead of require

--- ruby/texttest_fixture.rb
+++ ruby/texttest_fixture.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/ruby -w
 
- require_relative 'gilded_rose_impve.rb'
- require_relative 'item.rb'
+ require_relative 'gilded_rose.rb'
 # require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 puts "OMGHAI!"
